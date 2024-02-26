e8fb34800df81fba9f5d53a4c11c332b1c622b89
Sergei Lavrentev <23312691+lavrd@users.noreply.github.com>
2024-02-26 16:35:02 +0000

    Implemented tunnel server side

--- README.md
+++ README.md
@@ -11,3 +11,17 @@ cargo build ; sudo target/debug/tunnel tun0 10.0.0.1/24
 sudo ip route add 1.1.1.1/32 dev tun0
 ping 1.1.1.1 -c 1
 ```
+
+### Server
+
+```shell
+iptables-save
+sudo iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+
+sysctl net.ipv4.ip_forward
+sudo sysctl -w net.ipv4.ip_forward=1
+
+cargo build ; sudo target/debug/tunnel tun0 10.0.0.2/24
+```
+
+Trace IP packets: `tcpdump -v -i tun0 proto \\icmp`.
--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,60 @@
 use std::{
-    io::{ErrorKind, Read},
+    io::{ErrorKind, Read, Write},
+    net::TcpListener,
     sync::mpsc,
     thread,
     time::Duration,
 };
 
+use etherparse::{IpNumber, Ipv4Slice};
 use signal_hook::{consts::TERM_SIGNALS, iterator::Signals};
 
 mod ioctl;
 mod linux;
 
+const MTU: usize = 512;
+
 fn main() -> std::io::Result<()> {
     let name = std::env::args().nth(1).ok_or_else(|| new_io_err("failed to find arg with name"))?;
     let ip = std::env::args().nth(2).ok_or_else(|| new_io_err("failed to find arg with ip"))?;
     eprintln!("Starting TUN device with '{}' name and '{}' IP", name, ip);
+
     let (stop_s, stop_r) = mpsc::channel::<()>();
     let (stop_cb_s, stop_cb_r) = mpsc::channel::<()>();
+
     thread::spawn(move || -> std::io::Result<()> {
-        let iface = linux::Interface::new(name, &[ip.parse().map_err(map_io_err)?], 512)?;
+        let iface = linux::Interface::new(name, &[ip.parse().map_err(map_io_err)?], MTU)?;
         let mut tun_fd = iface.into_tun_fd();
+
+        let listener = TcpListener::bind("0.0.0.0:6688")?;
+        let (mut socket, _) = listener.accept()?;
+        let mut buffer = vec![0; MTU];
+        let n = socket.read(&mut buffer)?;
+        let _ = tun_fd.write(&buffer[..n])?;
+
         loop {
             if stop_r.try_recv().is_ok() {
                 stop_cb_s.send(()).map_err(map_io_err)?;
                 return Ok(());
             }
-            let mut buffer = vec![0; 512];
+            let mut buffer = vec![0; MTU];
             match tun_fd.read(&mut buffer) {
                 Ok(n) => {
-                    eprintln!("Received packet: {:?}", &buffer[..n]);
-                    continue;
+                    let buffer = &buffer[..n];
+                    eprintln!("Received packet: {:?}", buffer);
+                    let ipv4_packet = Ipv4Slice::from_slice(buffer).map_err(map_io_err)?;
+                    if ipv4_packet.header().protocol() != IpNumber::ICMP {
+                        continue;
+                    }
+                    let _ = socket.write(buffer)?;
+                    std::process::exit(0);
                 }
                 Err(ref e) if e.kind() == ErrorKind::WouldBlock => continue,
                 Err(e) => return Err(e),
             }
         }
     });
+
     let mut sigs = Signals::new(TERM_SIGNALS)?;
     sigs.into_iter().next().ok_or_else(|| new_io_err("failed to wait for termination signal"))?;
     eprintln!("Received termination signal");
