19ecb218c3afb9b81145a9da0c9441e0fd27db8b
Sergei Lavrentev <23312691+lavrd@users.noreply.github.com>
2024-02-26 16:45:21 +0000

    Implemented tunnel client side

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 use std::{
+    fs::File,
     io::{ErrorKind, Read, Write},
-    net::TcpListener,
-    sync::mpsc,
+    net::{TcpListener, TcpStream},
+    sync::mpsc::{channel, Receiver, Sender},
     thread,
     time::Duration,
 };
@@ -14,44 +15,26 @@ mod linux;
 
 const MTU: usize = 512;
 
+const SIDE_CLIENT: &str = "client";
+const SIDE_SERVER: &str = "server";
+
 fn main() -> std::io::Result<()> {
     let name = std::env::args().nth(1).ok_or_else(|| new_io_err("failed to find arg with name"))?;
     let ip = std::env::args().nth(2).ok_or_else(|| new_io_err("failed to find arg with ip"))?;
-    eprintln!("Starting TUN device with '{}' name and '{}' IP", name, ip);
+    let side = std::env::args().nth(3).ok_or_else(|| new_io_err("failed to find arg with side"))?;
+    eprintln!("Starting TUN device with '{}' name and '{}' IP on {}", name, ip, side);
 
-    let (stop_s, stop_r) = mpsc::channel::<()>();
-    let (stop_cb_s, stop_cb_r) = mpsc::channel::<()>();
+    let (stop_s, stop_r) = channel::<()>();
+    let (stop_cb_s, stop_cb_r) = channel::<()>();
 
     thread::spawn(move || -> std::io::Result<()> {
         let iface = linux::Interface::new(name, &[ip.parse().map_err(map_io_err)?], MTU)?;
-        let mut tun_fd = iface.into_tun_fd();
+        let tun_fd = iface.into_tun_fd();
 
-        let listener = TcpListener::bind("0.0.0.0:6688")?;
-        let (mut socket, _) = listener.accept()?;
-        let mut buffer = vec![0; MTU];
-        let n = socket.read(&mut buffer)?;
-        let _ = tun_fd.write(&buffer[..n])?;
-
-        loop {
-            if stop_r.try_recv().is_ok() {
-                stop_cb_s.send(()).map_err(map_io_err)?;
-                return Ok(());
-            }
-            let mut buffer = vec![0; MTU];
-            match tun_fd.read(&mut buffer) {
-                Ok(n) => {
-                    let buffer = &buffer[..n];
-                    eprintln!("Received packet: {:?}", buffer);
-                    let ipv4_packet = Ipv4Slice::from_slice(buffer).map_err(map_io_err)?;
-                    if ipv4_packet.header().protocol() != IpNumber::ICMP {
-                        continue;
-                    }
-                    let _ = socket.write(buffer)?;
-                    std::process::exit(0);
-                }
-                Err(ref e) if e.kind() == ErrorKind::WouldBlock => continue,
-                Err(e) => return Err(e),
-            }
+        match side.as_str() {
+            SIDE_CLIENT => client(tun_fd, stop_r, stop_cb_s),
+            SIDE_SERVER => server(tun_fd, stop_r, stop_cb_s),
+            _ => Ok(()),
         }
     });
 
@@ -69,6 +52,60 @@ pub(crate) fn map_io_err<T: ToString>(e: T) -> std::io::Error {
 
 pub(crate) fn map_io_err_msg<T: ToString>(e: T, msg: &str) -> std::io::Error {
     std::io::Error::new(std::io::ErrorKind::Other, format!("{}: {}", e.to_string(), msg))
+}
+
+fn client(mut tun_fd: File, stop_r: Receiver<()>, stop_cb_s: Sender<()>) -> std::io::Result<()> {
+    loop {
+        if stop_r.try_recv().is_ok() {
+            stop_cb_s.send(()).map_err(map_io_err)?;
+            return Ok(());
+        }
+        let mut buffer = vec![0; MTU];
+        match tun_fd.read(&mut buffer) {
+            Ok(n) => {
+                let buffer = &buffer[..n];
+                eprintln!("Received packet: {:?}", buffer);
+                let mut socket = TcpStream::connect("164.92.207.87:6688")?;
+                let _ = socket.write(buffer)?;
+                let mut buffer = vec![0; 512];
+                let n = socket.read(&mut buffer)?;
+                let buffer = &buffer[..n];
+                let _ = tun_fd.write(buffer)?;
+                std::process::exit(0);
+            }
+            Err(ref e) if e.kind() == ErrorKind::WouldBlock => continue,
+            Err(e) => return Err(e),
+        }
+    }
+}
+
+fn server(mut tun_fd: File, stop_r: Receiver<()>, stop_cb_s: Sender<()>) -> std::io::Result<()> {
+    let listener = TcpListener::bind("0.0.0.0:6688")?;
+    let (mut socket, _) = listener.accept()?;
+    let mut buffer = vec![0; MTU];
+    let n = socket.read(&mut buffer)?;
+    let _ = tun_fd.write(&buffer[..n])?;
+    loop {
+        if stop_r.try_recv().is_ok() {
+            stop_cb_s.send(()).map_err(map_io_err)?;
+            return Ok(());
+        }
+        let mut buffer = vec![0; MTU];
+        match tun_fd.read(&mut buffer) {
+            Ok(n) => {
+                let buffer = &buffer[..n];
+                eprintln!("Received packet: {:?}", buffer);
+                let ipv4_packet = Ipv4Slice::from_slice(buffer).map_err(map_io_err)?;
+                if ipv4_packet.header().protocol() != IpNumber::ICMP {
+                    continue;
+                }
+                let _ = socket.write(buffer)?;
+                std::process::exit(0);
+            }
+            Err(ref e) if e.kind() == ErrorKind::WouldBlock => continue,
+            Err(e) => return Err(e),
+        }
+    }
 }
 
 fn new_io_err(msg: &str) -> std::io::Error {
