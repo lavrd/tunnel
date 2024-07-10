2532fd3c020e2ae176427ecf578448c3c5b73696
Sergei Lavrentev <23312691+lavrd@users.noreply.github.com>
2024-07-10 04:23:51 +0000

    Change default tun iface ips

--- run_tun_docker.sh
+++ run_tun_docker.sh
@@ -4,11 +4,11 @@ if [ "$SERVER" == "1" ]; then
     iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
     ./tunnel run \
         ${TUNNEL_PRIVATE_KEY} ${CLIENT_PUBLIC_KEY} \
-        --tun-iface-name tun0 --tun-iface-ip 10.0.0.8/24 &
+        --tun-iface-name tun0 --tun-iface-ip 10.0.0.2/24 &
 elif [ "$CLIENT" == "1" ]; then
     ./tunnel run \
         ${TUNNEL_PRIVATE_KEY} ${CLIENT_PUBLIC_KEY} \
-        --tun-iface-name tun1  --tun-iface-ip 10.0.0.9/24 \
+        --tun-iface-name tun1 --tun-iface-ip 10.0.0.3/24 \
         --udp-server-ip ${SERVER_DOCKER_IP} &
     sleep 1 # to wait until tun1 interface will be up
     ip route add 1.1.1.1/32 dev tun1 &
